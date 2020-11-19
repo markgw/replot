052500a2c92bd751b9c6290e0b23cedcf596ef0e
Mark Granroth-Wilding <4409810+markgw@users.noreply.github.com>
2020-11-19 12:10:46 +0200

    Fixed setup.py so replot gets installed

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='replot',
     version='0.1',
-    packages=find_packages(include=['replot', 'replot.*'])
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        "matplotlib"
+    ],
 )
